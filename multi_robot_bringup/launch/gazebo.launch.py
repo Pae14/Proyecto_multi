ebc7e91e53aaaf69cd5d2bdd8ce0f4b42e87aeea
Ruth <rcm147@alu.ua.com>
2026-04-28 17:28:41 +0200

    Ya se ve el rover en el mundo, tambien se mueve con el comando de la terminal

--- multi_robot_bringup/launch/gazebo.launch.py
+++ multi_robot_bringup/launch/gazebo.launch.py
@@ -4,6 +4,10 @@ from launch import LaunchDescription
 from launch.actions import ExecuteProcess, SetEnvironmentVariable
 from launch_ros.actions import Node
 
+from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import PathJoinSubstitution, Command
+from launch_ros.parameter_descriptions import ParameterValue
+
 def generate_launch_description():
 
     pkg_path = get_package_share_directory('multi_robot_bringup')
@@ -20,28 +24,61 @@ def generate_launch_description():
     world = os.path.join(pkg_path, 'world', 'myworld.world')
     world_name = 'bosque_ruinas_mezcla'
 
+    robot_description = ParameterValue(
+        Command([
+            'xacro ',
+            PathJoinSubstitution([
+                FindPackageShare('rover_description'),
+                'urdf',
+                'rover.xacro'
+            ])
+        ]),
+        value_type=str
+    )
+    robot=Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'robot_description': robot_description}]
+    )
+    gazebo = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=[
+            '-topic', 'robot_description',
+            '-name', 'rover',
+            '-x', '0',
+            '-y', '0',
+            '-z', '0.1'
+        ],
+        output='screen'
+    )
+
     # --- BRIDGES (PUENTES) PARA LOS ROBOTS PROFESIONALES ---
     bridge = Node(
         package='ros_gz_bridge',
         executable='parameter_bridge',
         arguments=[
             '/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock',
-            # EXPLORER R2 (Rover)
-            '/model/rover_explorer/cmd_vel@geometry_msgs/msg/Twist]gz.msgs.Twist',
-            '/model/rover_explorer/odometry@nav_msgs/msg/Odometry[gz.msgs.Odometry',
-            # CERBERUS M100 (Dron)
-            '/model/uav_cerberus/cmd_vel@geometry_msgs/msg/Twist]gz.msgs.Twist',
-            # Cámara del Cerberus (Ajustado según jerarquía típica de estos modelos)
-            '/world/bosque_ruinas_mezcla/model/uav_cerberus/link/base_link/sensor/camera/image@sensor_msgs/msg/Image[gz.msgs.Image',
+
+            # ROVER (corregido)
+            '/model/rover/cmd_vel@geometry_msgs/msg/Twist]gz.msgs.Twist',
+            '/model/rover/odometry@nav_msgs/msg/Odometry[gz.msgs.Odometry',
+
+            # DRON (déjalo si existe)
+            #'/model/uav_cerberus/cmd_vel@geometry_msgs/msg/Twist]gz.msgs.Twist',
+
+            f'/world/{world_name}/model/uav_cerberus/link/base_link/sensor/camera/image@sensor_msgs/msg/Image[gz.msgs.Image',
         ],
         remappings=[
-            ('/model/rover_explorer/cmd_vel', '/rover/cmd_vel'),
-            ('/model/rover_explorer/odometry', '/rover/odom'),
+            ('/model/rover/cmd_vel', '/rover/cmd_vel'),
+            ('/model/rover/odometry', '/rover/odom'),
+
             ('/model/uav_cerberus/cmd_vel', '/uav/cmd_vel'),
             (f'/world/{world_name}/model/uav_cerberus/link/base_link/sensor/camera/image', '/uav/camera/image_raw'),
         ],
         output='screen'
     )
+
 
     return LaunchDescription([
         SetEnvironmentVariable(name='GZ_SIM_RESOURCE_PATH', value=gz_resource_path),
@@ -50,6 +87,7 @@ def generate_launch_description():
             cmd=['gz', 'sim', '-r', world],
             output='screen'
         ),
-
+        robot,
+        gazebo,
         bridge
     ])
