b2f51154281e071d91466ee5a02e4f4093d13f87
luciia-04 <lcd26@alu.ua.es>
2026-05-09 13:33:15 +0200

    Movimiento en gazebo del brazo encima del rover

--- prueba/prueba/prueba_comunicacion.py
+++ prueba/prueba/prueba_comunicacion.py
@@ -8,42 +8,44 @@ import std_msgs.msg
 class PuenteComunicacion(Node):
     def __init__(self):
         super().__init__('puente_abb')
-        #Publica el estado de las articulaciones para ver como se mueven en rviz
-        self.joint_pub = self.create_publisher(JointState, 'joint_states', 10)
 
-        #Publica el estado de las articulaciones para gazebo
-        self.gz_pub = self.create_publisher(JointTrajectory, '/arm_controller/joint_trajectory', 10)
+        #Publicador para las trayectorias para gazebo
+        self.gz_pub = self.create_publisher(JointTrajectory, '/rover/arm_controller/joint_trajectory', 10)
         
+        #Configuracion del socket
         self.s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.ip="172.26.112.1"
         self.puerto=5501
 
         try:
+            #Conexión del socket
             self.s.connect((self.ip, self.puerto))
             self.s.settimeout(10.0)
             saludo = self.s.recv(1024).decode()
             self.get_logger().info(f"RobotStudio dice: {saludo}")
             self.s.send("OBJETO_DISPONIBLE".encode())
+            #Socket no bloqueante para no congelar el nodo de ros mientras espera datos
             self.s.setblocking(False)
             self.get_logger().info(f"Conectado a RobotStudio en {self.ip}")
         except Exception as e:
             self.get_logger().error(f"Error de conexión: {e}")
 
+        #Leer el socket cada 10ms
         self.timer = self.create_timer(0.01, self.leer_socket)
 
 
     def leer_socket(self):
         try:
-            # Intentamos recibir datos
+            #Recibimos los del robotstudio datos
             data = self.s.recv(1024).decode()
             if data:
-                if "HECHO" in data:
+                if "HECHO" in data: #si en el mensaje viene la cadena "HECHO"
                     self.get_logger().info("Detectado fin de trayectoria (HECHO)")
-                    data_limpia = data.replace("HECHO", "")
+                    data_limpia = data.replace("HECHO", "") #limpiamos el HECHO para no perder la posición
                     if data_limpia:
                         self.publicar_articulaciones(data_limpia)
                 else:
-                    self.publicar_articulaciones(data)
+                    self.publicar_articulaciones(data) #publicar
         except (BlockingIOError, socket.error):
             pass
         except Exception as e:
@@ -51,27 +53,31 @@ class PuenteComunicacion(Node):
 
     
     def publicar_articulaciones(self, data):
-        msg = JointState()
-        msg.header.stamp = self.get_clock().now().to_msg()
-        msg.name = ['joint_1', 'joint_2', 'joint_3', 'joint_4', 'joint_5', 'joint_6']
-
+        #Instancia de mensaje de trayectoria
         traj_msg = JointTrajectory()
-        traj_msg.joint_names = msg.name
+        joint_names= ['arm_joint_1', 'arm_joint_2', 'arm_joint_3', 'arm_joint_4', 'arm_joint_5', 'arm_joint_6']
+        traj_msg.joint_names = joint_names
+
+        #Creación del punto de trayectoria
         point = JointTrajectoryPoint()
 
         try:
             angulos_grados = [float(x) for x in data.split(',')] #Paso de string a float
             radianes = [x * (3.14159 / 180.0) for x in angulos_grados] #ABB usa grados y ROS radianes --> pasar a radianes
-            
-            #Publicar en Rviz
-            msg.position = radianes
-            self.joint_pub.publish(msg)
 
-            #Publicar en Gazebo
-            point.positions=radianes
-            point.time_from_start.sec = 2
-            traj_msg.points.append(point)
+            point.positions = radianes  #se define el punto objetivo
+            point.time_from_start.sec = 0
+            point.time_from_start.nanosec = 300000000 #0.3 seg para alcanzar la posicion
+
+            #Esto es para que la ejecución sea instantánea
+            traj_msg.header.stamp.sec = 0  
+            traj_msg.header.stamp.nanosec = 0
+            #Se añade el punto a la lista de puntos de la trayectoria
+            traj_msg.points = [point]
+
+            #envío del mensaje al controlador del brazo
             self.gz_pub.publish(traj_msg)
+            self.get_logger().info(f"Publicando: {[f'{r:.3f}' for r in radianes]}")
 
         except ValueError as e:
             self.get_logger().warn(f"Datos mal formateados: {data} → {e}")
